2d461be7a3786ba8b433108e5c52da71a254cae9
Leon Franke <lf318937@fh-muenster.de>
2019-04-22 14:24:11 +0200

    einfügen einer hexToDecFunktion für das decoden des URLS
    Anlegen der HexToDec funktion(Noch in Arbeit)

--- src/http_utils.c
+++ src/http_utils.c
@@ -1,1 +1,46 @@
 #include "../include/http_utils.h"
+
+
+
+int hexToDec(string *hex){
+
+    unsigned int val = 0;
+
+    for(unsigned int i=0;i<hex->len;i++)
+        if(hex->str[i] <= 57)
+            val += (hex->str[i]-48)*(1<<(4*(hex->len-1-i)));
+        else
+            val += (hex->str[i]-55)*(1<<(4*(hex->len-1-i)));
+
+    return val;
+
+
+}
+
+
+string *decodeURL(string *url){
+
+
+    for(int i = 0; i < url->len; i++){
+
+        if(url->str[i] == '%'){
+
+            //TODO wenn str replace done ist
+
+
+
+        }
+
+
+    }
+
+
+}
+
+
+
+
+
+
+
+
