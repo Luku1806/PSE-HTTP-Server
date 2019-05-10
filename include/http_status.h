ab9fc3a4d10aa875f33ae9f35a87331caecd73a1
Florian Galz <fg246385@fh-muenster.de>
2019-05-10 14:36:12 +0200

    Status 505 für HTTP Versions Überprüfung hinzugefügt

--- include/http_status.h
+++ include/http_status.h
@@ -3,24 +3,25 @@
 
 #include "http_string.h"
 
-#define HTTP_STATUS_OK                      200
-#define HTTP_STATUS_CREATED                 201
-#define HTTP_STATUS_ACCEPTED                202
-#define HTTP_STATUS_NO_CONTENT              204
+#define HTTP_STATUS_OK                              200
+#define HTTP_STATUS_CREATED                         201
+#define HTTP_STATUS_ACCEPTED                        202
+#define HTTP_STATUS_NO_CONTENT                      204
 
-#define HTTP_STATUS_MOVED_PERMANENTLY       301
-#define HTTP_STATUS_MOVED_TEMPORARELY       302
-#define HTTP_STATUS_NOT_MODIFIED            304
+#define HTTP_STATUS_MOVED_PERMANENTLY               301
+#define HTTP_STATUS_MOVED_TEMPORARELY               302
+#define HTTP_STATUS_NOT_MODIFIED                    304
 
-#define HTTP_STATUS_BAD_REQUEST             400
-#define HTTP_STATUS_UNAUTHORIZED            401
-#define HTTP_STATUS_FORBIDDEN               403
-#define HTTP_STATUS_NOT_FOUND               404
+#define HTTP_STATUS_BAD_REQUEST                     400
+#define HTTP_STATUS_UNAUTHORIZED                    401
+#define HTTP_STATUS_FORBIDDEN                       403
+#define HTTP_STATUS_NOT_FOUND                       404
 
-#define HTTP_STATUS_INTERNAL_SERVER_ERROR   500
-#define HTTP_STATUS_NOT_IMPLEMENTED         501
-#define HTTP_STATUS_BAD_GATEWAY             502
-#define HTTP_STATUS_SERVICE_UNAVAILABLE     503
+#define HTTP_STATUS_INTERNAL_SERVER_ERROR           500
+#define HTTP_STATUS_NOT_IMPLEMENTED                 501
+#define HTTP_STATUS_BAD_GATEWAY                     502
+#define HTTP_STATUS_SERVICE_UNAVAILABLE             503
+#define HTTP_STATUS_SERVICE_VERSION_NOT_SUPPORTED   503
 
 
 
