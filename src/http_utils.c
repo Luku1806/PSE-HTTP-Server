fcfd4987e7e5c4f2aab70becbea95daf5e0e46b8
Lukas Reining <lr413680@fh-muenster.de>
2019-05-17 10:16:01 +0200

    URL Decoding angepasst, fehlerhafte URLs werden erkannt und können weitergehend behandelt werden.

--- src/http_utils.c
+++ src/http_utils.c
@@ -1,13 +1,20 @@
 #include "../include/http_utils.h"
 
 
+char isAscii(char c) {
+    return (48 <= c && c <= 57) || (65 <= c && c <= 70) || (97 <= c && c <= 102);
+}
+
+
 char hexStringToChar(string *hex) {
     char val = 0;
 
     for (size_t i = 0; i < hex->len; i++) {
-        if (hex->str[i] <= 57) {
+        if (!isAscii(hex->str[i])) {
+            return -1;
+        } else if (hex->str[i] <= 57) {
             val += (hex->str[i] - 48) * (1 << (4 * (hex->len - 1 - i)));
-        }else {
+        } else {
             val += (hex->str[i] - 55) * (1 << (4 * (hex->len - 1 - i)));
         }
     }
@@ -22,8 +29,14 @@ string *decodeURL(string *url) {
 
     for (size_t i = 0; i < url->len; i++) {
 
-        if (url->str[i] == '%') {
+        if (url->str[i] == '%' && i < url->len - 2) {
             string *numbers = sub_str(url, i + 1, 2);
+
+            if (!(isAscii(numbers->str[0]) && isAscii(numbers->str[1]))) {
+                free(decodedChars);
+                free_str(numbers);
+                return NULL; // Digits after the percent are not hex --> INVALID
+            }
 
             int numbersDecimal = hexStringToChar(numbers);
             decodedChars[copyIndex++] = numbersDecimal;
@@ -31,10 +44,14 @@ string *decodeURL(string *url) {
             free_str(numbers);
             i += 2;
             continue;
+        } else if (url->str[i] == '%') {
+            free(decodedChars);
+            return NULL; // Percent in one of the last two characters --> INVALID
         }
 
         decodedChars[copyIndex++] = url->str[i];
     }
+
     decodedChars[copyIndex] = '\0';
 
     string *decoded = cpy_str(decodedChars);
