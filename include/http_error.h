7501f204e60f00761d00b9f531038b74c24bb1b2
Leon Franke <lf318937@fh-muenster.de>
2019-05-18 15:38:16 +0200

    Hinzufügen der @brief Beschreibungen

--- include/http_error.h
+++ include/http_error.h
@@ -1,6 +1,11 @@
 #ifndef HTTP_SERVER_09_HTTP_ERROR_H
 #define HTTP_SERVER_09_HTTP_ERROR_H
 
+/**
+ *
+ * @brief Here we evaluate the status message and answer accordingly.
+ */
+
 
 /**
  * Prints a message to the standard error output and exits.
