9a700b2833638a3571596b45fe62d68ede20271d
Leon Franke <lf318937@fh-muenster.de>
2019-05-21 16:39:02 +0200

    Anlegen der security.h und .c Datei

---
+++ include/http_security.h
@@ -0,0 +1,10 @@
+#ifndef HTTP_SERVER_09_HTTP_SECURITY_H
+#define HTTP_SERVER_09_HTTP_SECURITY_H
+
+#include "http_string.h"
+#include "http_parser.h"
+
+
+
+
+#endif //HTTP_SERVER_09_HTTP_SECURITY_H
---
+++ src/http_security.c
@@ -0,0 +1,10 @@
+#include "../include/http_security.h"
+#include "../include/http_string.h"
+#include "../include/http_error.h"
+#include "../include/http_parser.h"
+#include "../include/http_base64.h"
+
+#include <stdlib.h>
+#include <openssl/sha.h>
+#include <stdio.h>
+
