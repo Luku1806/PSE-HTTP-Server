4b8e86218d31427801b453d566cad86fe268aa8b
Florian Galz <fg246385@fh-muenster.de>
2019-05-21 15:30:07 +0200

    Implementierung von base64_encode Funktion

--- src/http_base64.c
+++ src/http_base64.c
@@ -31,6 +31,33 @@ void base64_cleanup() {
     decoding_table = NULL;
 }
 
+char *base64_encode(const unsigned char *data, size_t input_length, size_t *output_length) {
+    *output_length = 4 * ((input_length + 2) / 3);
+
+    char *encoded_data = malloc(*output_length + 1);
+    if (encoded_data == NULL) return NULL;
+
+    for (int i = 0, j = 0; i < input_length;) {
+
+        uint32_t octet_a = i < input_length ? data[i++] : 0;
+        uint32_t octet_b = i < input_length ? data[i++] : 0;
+        uint32_t octet_c = i < input_length ? data[i++] : 0;
+
+        uint32_t triple = (octet_a << 0x10) + (octet_b << 0x08) + octet_c;
+
+        encoded_data[j++] = encoding_table[(triple >> 3 * 6) & 0x3F];
+        encoded_data[j++] = encoding_table[(triple >> 2 * 6) & 0x3F];
+        encoded_data[j++] = encoding_table[(triple >> 1 * 6) & 0x3F];
+        encoded_data[j++] = encoding_table[(triple >> 0 * 6) & 0x3F];
+    }
+
+    for (int i = 0; i < mod_table[input_length % 3]; i++) {
+        encoded_data[*output_length - 1 - i] = '=';
+    }
+    encoded_data[*output_length] = '\0';
+
+    return encoded_data;
+}
 
 string *base64_encode_string(string *toEncode) {
     size_t newLen;
